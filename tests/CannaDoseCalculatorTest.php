d6c8c868f5e8b2433d1e0e12a382a7a792490ff5
Dennis van den Brock <dennis.vandenbrock@easybill.de>
2024-04-30 19:45:45 +0200

    Added Test

---
+++ tests/CannaDoseCalculatorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests;
+
+use App\Services\CannaDoseCalculatorService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class CannaDoseCalculatorTest extends KernelTestCase
+{
+    public function testSomething(): void
+    {
+        $container = static::getContainer();
+
+        $cannaDoseCalculatorService = $container->get(CannaDoseCalculatorService::class);
+
+        $cannaDoseCalculatorData = [
+            'basis_dosage' => 300,
+            'experience' => 0.8,
+            'intensity' => 10,
+        ];
+
+        $dose = $cannaDoseCalculatorService->calculateDose($cannaDoseCalculatorData);
+
+        $this->assertSame(354.0, $dose, 'The calculated dose is not correct.');
+    }
+}
