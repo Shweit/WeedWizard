b8ed82750f09345f9578afceb64da3cb4267dd3d
Dennis van den Brock <dennis.vandenbrock@easybill.de>
2024-04-30 19:36:13 +0200

    CannaDose Calculator

---
+++ assets/script/CannaDoseCalculator.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const intensity = document.getElementById('canna_dose_calculator_intensity');
+    intensity.addEventListener('input', function() {
+
+        let value = intensity.value;
+
+        switch (true) {
+            case value >= 1 && value <= 3:
+                value = 'Sehr Niedrig';
+                break;
+            case value >= 4 && value <= 8:
+                value = 'Niedrig';
+                break;
+            case value >= 9 && value <= 12:
+                value = 'Mittel';
+                break;
+            case value >= 13 && value <= 16:
+                value = 'Hoch';
+                break;
+            case value >= 17 && value <= 20:
+                value = 'Sehr hoch';
+                break;
+        }
+
+        document.getElementById('intensity-value').textContent = value;
+    });
+});
