4910b6510fab71efe80d925d6aea20d1b9000408
Dennis van den Brock <dennis.vandenbrock@easybill.de>
2024-05-02 10:06:17 +0200

    Fix test

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,8 +11,6 @@ class IndexController extends AbstractController
     #[Route('/', name: 'weedwizard_index')]
     public function index(): Response
     {
-        dd($this->getUser());
-
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
