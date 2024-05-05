c8f0ef55aa84b8b8c27bf976a68d764c5f7ccecd
flix29 <flix.1895@web.de>
2024-05-05 22:48:56 +0200

    fix PHP stan errors

--- src/Controller/CannabisVereinController.php
+++ src/Controller/CannabisVereinController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\CannabisVerein;
 use App\Form\CannabisVereinType;
-use App\Repository\CannabisVereinRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,19 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CannabisVereinController extends AbstractController
 {
-    private CannabisVereinRepository $cannabisVereinRepository;
-
     public function __construct(
         private readonly EntityManagerInterface $entityManager
-    ) {
-        $this->cannabisVereinRepository = $this->entityManager->getRepository(CannabisVerein::class);
-    }
+    ) {}
 
     #[Route('/cannabis-verein', name: 'app_cannabis_verein')]
     public function index(): Response
     {
         $form = $this->createForm(CannabisVereinType::class);
-        $cannabisVereine = $this->cannabisVereinRepository->findAll();
+        $cannabisVereine = $this->entityManager->getRepository(CannabisVerein::class)->findAll();
 
         return $this->render('cannabis_verein/index.html.twig', [
             'form' => $form->createView(),
