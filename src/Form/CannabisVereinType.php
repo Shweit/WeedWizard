4822eccbec69b2a6e32f25e94d262f225ec2790a
flix29 <flix.1895@web.de>
2024-05-10 22:58:59 +0200

    meet cs fixer goals

--- src/Form/CannabisVereinType.php
+++ src/Form/CannabisVereinType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\CannabisVerein;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
