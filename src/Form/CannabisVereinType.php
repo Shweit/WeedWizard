da6981df36a6ca4efa198fe81fc4fca2fa108727
flix29 <flix.1895@web.de>
2024-05-10 22:48:45 +0200

    display form

--- src/Form/CannabisVereinType.php
+++ src/Form/CannabisVereinType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\CannabisVerein;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -13,6 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CannabisVereinType extends AbstractType
@@ -29,9 +31,21 @@ class CannabisVereinType extends AbstractType
                     ),
                 ],
             ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'constraints' => [
+                    new NotBlank(message: 'Bitte gib eine Adresse ein.'),
+                    new Length(['min' => 3, 'max' => 255]),
+                ],
+            ])
+            ->add('mapbox_id', HiddenType::class, [
+                'mapped' => false,
+                'required' => false,
+            ])
             ->add('strasse', TextType::class, [
                 'required' => true,
                 'label' => 'Straße',
+                'disabled' => true,
                 'constraints' => [
                     new NotBlank(
                         message: 'Bitte gib eine gültige Straße ein.',
@@ -41,6 +55,7 @@ class CannabisVereinType extends AbstractType
             ->add('hausnummer', TextType::class, [
                 'required' => true,
                 'label' => 'Hausnummer',
+                'disabled' => true,
                 'constraints' => [
                     new NotBlank(
                         message: 'Bitte gib eine gültige Hausnummer ein.',
@@ -48,16 +63,25 @@ class CannabisVereinType extends AbstractType
                 ],
             ])
             ->add('plz', IntegerType::class, [
+                'mapped' => false,
                 'required' => true,
                 'label' => 'PLZ',
+                'disabled' => true,
                 'constraints' => [
                     new NotBlank(
                         message: 'Bitte gib eine gültige PLZ ein.',
                     ),
+                    new Length(
+                        min: 5,
+                        max: 5,
+                        minMessage: 'Die PLZ muss genau 5 Ziffern lang sein.',
+                        maxMessage: 'Die PLZ muss genau 5 Ziffern lang sein.',
+                    )
                 ],
             ])
             ->add('ort', TextType::class, [
                 'required' => true,
+                'disabled' => true,
                 'label' => 'Ort',
                 'constraints' => [
                     new NotBlank(
@@ -76,6 +100,7 @@ class CannabisVereinType extends AbstractType
             ->add('mitgliedsbeitrag', MoneyType::class, [
                 'required' => true,
                 'label' => 'Mitgliedsbeitrag',
+                'currency' => 'EUR',
                 'constraints' => [
                     new NotBlank(
                         message: 'Bitte gib einen gültigen Mitgliedsbeitrag ein.',
@@ -92,9 +117,6 @@ class CannabisVereinType extends AbstractType
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Verein erstellen',
-            ])
-            ->add('cancel', ButtonType::class, [
-                'label' => 'Abbrechen',
             ]);
     }
 
