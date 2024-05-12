ef3816346a98f293d792b17bc017d2f4cb22d0b9
jakob-pfefferkorn <jakob.pfefferkorn@vodafone.com>
2024-05-12 19:38:38 +0200

    Create CannaStrain library Controller

---
+++ src/Controller/CannaStrainLibraryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CannaStrainLibraryController extends AbstractController
+{
+    #[Route('/cannastrainlibrary', name: 'app_cannastrainlibrary')]
+    public function index(): Response
+    {
+        return $this->render('canna_strain_library/index.html.twig', [
+            'controller_name' => 'CannaStrainLibraryController',
+        ]);
+    }
+}
