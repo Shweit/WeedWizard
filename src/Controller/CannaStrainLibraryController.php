d615136d873fe5c27c23fcf3f25c6d95a5b1be2a
jakob-pfefferkorn <jakob.pfefferkorn@vodafone.com>
2024-05-13 17:09:49 +0200

    Add some filters and results elements to cannastrain library view

--- src/Controller/CannaStrainLibraryController.php
+++ src/Controller/CannaStrainLibraryController.php
@@ -8,11 +8,30 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CannaStrainLibraryController extends AbstractController
 {
+    protected array $filters = [
+        'filter' => [
+            'Herkunft' => [
+                'indica',
+                'sativa',
+                'ruderalis',
+                'unbekannt'],
+            'Strain-Typ' => [
+                'nur nicht femisierte',
+                'femisierte',
+                'close-only strains'],
+            'Location' => [
+                'Indoor',
+                'Outdoor',
+                'Gewächshaus'],
+        ],
+    ];
+
     #[Route('/cannastrainlibrary', name: 'weedwizard_cannastrainlibrary')]
     public function index(): Response
     {
         return $this->render('canna_strain_library/index.html.twig', [
             'controller_name' => 'CannaStrainLibraryController',
+            'filters' => $this->filters,
         ]);
     }
 }
