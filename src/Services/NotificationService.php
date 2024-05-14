2de7d995deab14c8c434284f7644c6ce618cfad3
Dennis van den Brock <72552171+Shweit@users.noreply.github.com>
2024-05-14 15:07:49 +0200

    Several changes and quick fixes (#38)
    - removed several `console.log()`
    - added logout redirect
    - added toast for notifications
    - added notification entity
    - added notification fixtures

---
+++ src/Services/NotificationService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+class NotificationService
+{
+    // Hier kann mam typisierte Konstanten definieren, welche für die Notification
+    // verwendet werden können.
+
+    public const CANNABIS_CONSUMPTION_COMPLIANCE_MAP_TYPE = 'cannabis_consumption_compliance_map';
+    public const CANNABIS_VEREINSSUCHE = 'cannabis_vereinssuche';
+    public const BUD_BASH_LOCATOR_TYPE = 'bud_bash_locator';
+    public const CANNASTRAIN_LIBRARY_TYPE = 'cannastrain_library';
+    public const CANNADOSE_CALCULATOR_TYPE = 'cannadose_calculator';
+    public const GROW_MATE_TYPE = 'grow_mate';
+    public const CANNA_CONSULTANT_TYPE = 'canna_consultant';
+}
