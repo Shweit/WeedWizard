0f169428680e9de11d6089801ca8938425e88580
flix29 <flix.1895@web.de>
2024-05-16 09:47:17 +0200

    meet goals

--- src/DataFixtures/ClubFixtures.php
+++ src/DataFixtures/ClubFixtures.php
@@ -19,7 +19,6 @@ class ClubFixtures extends Fixture
         $loremIpsum = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
         $hausnummer = 10;
 
-
         for ($i = 1; $i <= $hausnummer; ++$i) {
             $user = new User();
 
@@ -52,6 +51,7 @@ class ClubFixtures extends Fixture
     private function generateRandomEmail(): string
     {
         $email = rand(100, 100000);
-        return $email .= '@weedwizard.de';
+
+        return $email . '@weedwizard.de';
     }
 }
