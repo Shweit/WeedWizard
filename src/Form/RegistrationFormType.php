1c15353432898f10307dec327679d514383236c7
flix29 <flix.1895@web.de>
2024-06-13 14:25:21 +0200

    refine frontend

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -25,7 +25,12 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Vorname',
-                'attr' => ['autocomplete' => 'firstname'],
+                'attr' => [
+                    'autocomplete' => 'firstname',
+                    'class' => 'form-control rounded-5',
+                    'placeholder' => 'Vorname',
+                ],
+                'required' => true,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Bitte gib deinen Vornamen an.',
@@ -34,22 +39,35 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nachname',
-                'attr' => ['autocomplete' => 'lastname'],
+                'attr' => [
+                    'autocomplete' => 'lastname',
+                    'class' => 'form-control rounded-5',
+                    'placeholder' => 'Nachname',
+                ],
                 'required' => false,
             ])
             ->add('birthdate', DateType::class, [
                 'label' => 'Geburtstag',
-                'attr' => ['autocomplete' => 'birthdate'],
+                'attr' => [
+                    'autocomplete' => 'birthdate',
+                    'class' => 'form-control rounded-5',
+                    'placeholder' => 'Geburtsdatum',
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Bitte gib dein Geburtsdatum an.',
                     ]),
                     new LessThanOrEqual('-18 years', message: 'Du musst mindestens 18 Jahre alt sein.'),
                 ],
+                'required' => true,
             ])
             ->add('email', EmailType::class, [
                 'label' => 'E-Mail Adresse',
-                'attr' => ['autocomplete' => 'email'],
+                'attr' => [
+                    'autocomplete' => 'email',
+                    'class' => 'form-control rounded-5',
+                    'placeholder' => 'Email',
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Bitte gib eine E-Mail Adresse an.',
@@ -58,23 +76,39 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Die E-Mail Adresse "{{ value }}" ist keine gültige E-Mail Adresse.',
                     ]),
                 ],
+                'required' => true,
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'label' => 'Ich stimme den AGB\'s zu',
+                'attr' => ['class' => 'form-check-input'],
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'Du musst unseren AGB zustimmen.',
                     ]),
                 ],
+                'required' => true,
             ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Die Passwörter müssen übereinstimmen.',
-                'options' => ['attr' => ['autocomplete' => 'password']],
                 'required' => true,
-                'first_options' => ['label' => 'Passwort'],
-                'second_options' => ['label' => 'Passwort wiederholen'],
+                'first_options' => [
+                    'label' => 'Passwort',
+                    'attr' => [
+                        'autocomplete' => 'password',
+                        'class' => 'form-control rounded-5',
+                        'placeholder' => 'Passwort',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Passwort wiederholen',
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control rounded-5',
+                        'placeholder' => 'Passwort wiederholen',
+                    ],
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -85,8 +119,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
