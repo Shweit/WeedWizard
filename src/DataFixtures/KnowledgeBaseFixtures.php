78dd42ba9b46e303dd681a00e66d6a9b0a84f17d
Dennis van den Brock <dennis.vandenbrock@easybill.de>
2024-06-13 12:58:19 +0200

    added some standard entries to the KnowledgeBase

--- src/DataFixtures/KnowledgeBaseFixtures.php
+++ src/DataFixtures/KnowledgeBaseFixtures.php
@@ -12,44 +12,186 @@ class KnowledgeBaseFixtures extends Fixture
     {
         $entry = new KnowledgeBase();
         $entry->setSite('knowledge_base');
-        $entry->setCategorie('growing_tips');
-        $entry->setArticleName('How to grow weed');
-        $entry->setArticleContent('This is how you grow weed');
+        $entry->setCategorie('cannabis_tips');
+        $entry->setArticleName('Was ist Cannabis?');
+        $entry->setArticleContent('<p>Cannabis ist eine Pflanze, die für ihre psychoaktiven und medizinischen Eigenschaften bekannt ist. Die Hauptbestandteile von Cannabis sind Tetrahydrocannabinol (THC) und Cannabidiol (CBD). Während THC für die berauschende Wirkung verantwortlich ist, hat CBD keine psychoaktive Wirkung und wird häufig für medizinische Zwecke verwendet.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('cannabis_tips');
+        $entry->setArticleName('Wie wirkt Cannabis auf den Körper?');
+        $entry->setArticleContent('<p>Die Wirkung von Cannabis kann von Person zu Person unterschiedlich sein, hängt jedoch hauptsächlich von der Dosis und der Art des Konsums ab. Zu den häufigsten Effekten gehören Entspannung, veränderte Wahrnehmung, gesteigerter Appetit und Euphorie. Zu den möglichen Nebenwirkungen zählen Angstzustände, Paranoia und ein trockener Mund.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('cannabis_tips');
+        $entry->setArticleName('Welche Konsummethoden gibt es?');
+        $entry->setArticleContent('
+            <p>Es gibt verschiedene Methoden, Cannabis zu konsumieren, darunter:</p>
+            <ul>
+                <li><strong>Rauchen:</strong> Cannabis wird in einer Zigarette (Joint) oder einer Pfeife (Bong) geraucht.</li>
+                <li><strong>Verdampfen (Vaping):</strong> Cannabis wird erhitzt, bis die Wirkstoffe verdampfen, ohne das Pflanzenmaterial zu verbrennen.</li>
+                <li><strong>Esswaren (Edibles):</strong> Cannabis wird in Lebensmitteln oder Getränken verarbeitet.</li>
+                <li><strong>Öle und Tinkturen:</strong> Cannabisextrakte werden oral eingenommen oder unter die Zunge geträufelt.</li>
+            </ul>
+        ');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('cannabis_tips');
+        $entry->setArticleName('Was ist ein Strain?');
+        $entry->setArticleContent('<p>Ein Strain ist eine bestimmte Sorte von Cannabis, die sich durch einzigartige Merkmale wie Geruch, Geschmack und Wirkung auszeichnet. Strains werden oft nach ihrem genetischen Ursprung kategorisiert, wie Indica, Sativa oder Hybrid, und können unterschiedliche Mengen an THC und CBD enthalten.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('cannabis_tips');
+        $entry->setArticleName("Was bedeuten 'indica', 'sativa' und 'ruderalis'?");
+        $entry->setArticleContent('
+            <p>Indica, Sativa und Ruderalis sind die drei Hauptarten von Cannabis:</p>
+            <ul>
+              <li><strong>Indica:</strong> Diese Pflanzen sind meist kürzer und buschiger. Sie haben oft einen höheren CBD-Gehalt und werden mit entspannenden, sedierenden Effekten in Verbindung gebracht.</li>
+              <li><strong>Sativa:</strong> Diese Pflanzen sind größer und dünner. Sie haben oft einen höheren THC-Gehalt und werden mit belebenden, zerebralen Effekten in Verbindung gebracht.</li>
+              <li><strong>Ruderalis:</strong> Diese Pflanzen sind kleiner und robust. Sie werden selten allein verwendet, sondern oft mit Indica oder Sativa gekreuzt, um Autoflowering-Strains zu erzeugen.</li>
+            </ul>
+        ');
         $manager->persist($entry);
 
         $entry = new KnowledgeBase();
         $entry->setSite('knowledge_base');
         $entry->setCategorie('growing_tips');
-        $entry->setArticleName('How to grow weed');
-        $entry->setArticleContent('This is how you grow weed');
+        $entry->setArticleName('Was benötige ich, um Cannabis anzubauen?');
+        $entry->setArticleContent('
+            <p>Um Cannabis erfolgreich anzubauen, benötigen Sie folgende Grundausstattung:</p>
+            <ul>
+                <li><strong>Samen oder Setzlinge:</strong> Wählen Sie eine Sorte, die zu Ihrem Klima und Ihren Bedürfnissen passt.</li>
+                <li><strong>Beleuchtung:</strong> Für den Innenanbau benötigen Sie spezielle Lampen (z.B. LED, HPS).</li>
+                <li><strong>Anbaumedium:</strong> Erde, Kokosfaser oder Hydroponiksysteme.</li>
+                <li><strong>Nährstoffe:</strong> Spezielle Düngemittel für die Wachstums- und Blütephase.</li>
+                <li><strong>Lüftung:</strong> Ein Belüftungssystem, um die Luftzirkulation und Temperatur zu regulieren.</li>
+            </ul>
+        ');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('growing_tips');
+        $entry->setArticleName('Wie lange dauert der Anbau von Cannabis?');
+        $entry->setArticleContent('<p>Die Anbauzeit von Cannabis variiert je nach Sorte und Anbaumethode. Im Allgemeinen dauert es von der Keimung bis zur Ernte etwa 3 bis 5 Monate. Dies umfasst die Keimungsphase (1-2 Wochen), die Wachstumsphase (4-8 Wochen) und die Blütephase (6-12 Wochen).</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('growing_tips');
+        $entry->setArticleName('Welche Pflege benötigen die Pflanzen?');
+        $entry->setArticleContent('
+            <p>Um gesunde Cannabispflanzen zu züchten, müssen Sie Folgendes beachten:</p>
+            <ul>
+              <li><strong>Bewässerung:</strong> Gießen Sie regelmäßig, aber vermeiden Sie Staunässe.</li>
+              <li><strong>Nährstoffe:</strong> Düngen Sie die Pflanzen entsprechend ihrem Wachstumsstadium.</li>
+              <li><strong>Beleuchtung:</strong> Sorgen Sie für ausreichend Licht, insbesondere während der Wachstumsphase.</li>
+              <li><strong>Beschneidung und Training:</strong> Entfernen Sie abgestorbene Blätter und trainieren Sie die Pflanzen, um das Wachstum zu optimieren.</li>
+              <li><strong>Schädlingsbekämpfung:</strong> Überwachen Sie die Pflanzen auf Schädlinge und Krankheiten und handeln Sie bei Bedarf schnell.</li>
+            </ul>
+        ');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('growing_tips');
+        $entry->setArticleName('Was ist ein breeder/Züchter?');
+        $entry->setArticleContent('<p>Ein Breeder oder Züchter ist eine Person oder ein Unternehmen, das sich auf die Entwicklung neuer Cannabisstrains spezialisiert hat. Durch die Kreuzung verschiedener Pflanzen können Breeder spezifische Merkmale wie Potenz, Geschmack, Aroma und Wachstumseigenschaften hervorheben.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('growing_tips');
+        $entry->setArticleName('Was ist ein Seed?');
+        $entry->setArticleContent('<p>Ein Seed ist ein Samen, der zur Aufzucht von Cannabispflanzen verwendet wird. Samen können feminisiert sein, was bedeutet, dass sie mit hoher Wahrscheinlichkeit weibliche Pflanzen hervorbringen, die Blüten produzieren. Reguläre Samen können sowohl männliche als auch weibliche Pflanzen hervorbringen.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('growing_tips');
+        $entry->setArticleName('Was bedeutet (nicht) femisiert?');
+        $entry->setArticleContent('<p>Feminisierte Samen sind speziell gezüchtet, um fast ausschließlich weibliche Pflanzen hervorzubringen, die Blüten produzieren. Nicht-feminisierte oder reguläre Samen können sowohl männliche als auch weibliche Pflanzen hervorbringen. Männliche Pflanzen werden in der Regel entfernt, da sie keine Blüten produzieren und die weiblichen Pflanzen bestäuben können.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('growing_tips');
+        $entry->setArticleName('Was ist ein hybrid?');
+        $entry->setArticleContent('<p>Ein Hybrid ist eine Cannabispflanze, die durch Kreuzung von Indica- und Sativa-Pflanzen entstanden ist. Hybride können eine Mischung der Eigenschaften beider Arten haben und bieten oft eine ausgewogene Wirkung, die sowohl körperliche Entspannung als auch zerebrale Stimulation umfassen kann.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('growing_tips');
+        $entry->setArticleName('Was bedeutet close-only strain?');
+        $entry->setArticleContent("<p>Ein 'clone-only strain' ist eine Cannabissorte, die nur durch Klonen und nicht durch Samen vermehrt wird. Das bedeutet, dass die genetische Identität der Pflanze nur durch das Schneiden und Wurzeln eines Teils der Mutterpflanze erhalten bleibt. Dies stellt sicher, dass die spezifischen Merkmale der Sorte konsistent bleiben.</p>");
         $manager->persist($entry);
 
         $entry = new KnowledgeBase();
         $entry->setSite('knowledge_base');
         $entry->setCategorie('consuming_tips');
-        $entry->setArticleName('How to consume weed');
-        $entry->setArticleContent('This is how you consume weed');
+        $entry->setArticleName('Welche Risiken sind mit dem Cannabiskonsum verbunden?');
+        $entry->setArticleContent('
+            <p>Obwohl viele Menschen Cannabis ohne ernsthafte Probleme konsumieren, gibt es potenzielle Risiken:</p>
+            <ul>
+              <li><strong>Psychische Gesundheit:</strong> Hohe THC-Dosen können Angstzustände, Paranoia und psychotische Episoden auslösen, insbesondere bei gefährdeten Personen.</li>
+              <li><strong>Abhängigkeit:</strong> Langfristiger, regelmäßiger Konsum kann zu einer psychischen Abhängigkeit führen.</li>
+              <li><strong>Kognitive Funktionen:</strong> Bei Jugendlichen kann der Konsum die Gehirnentwicklung beeinträchtigen und zu langfristigen kognitiven Problemen führen.</li>
+              <li><strong>Körperliche Gesundheit:</strong> Rauchen von Cannabis kann die Lungen schädigen, ähnlich wie Tabakrauch.</li>
+            </ul>
+        ');
         $manager->persist($entry);
 
         $entry = new KnowledgeBase();
         $entry->setSite('knowledge_base');
-        $entry->setCategorie('cannabis_tips');
-        $entry->setArticleName('How to use cannabis');
-        $entry->setArticleContent('This is how you use cannabis');
+        $entry->setCategorie('consuming_tips');
+        $entry->setArticleName('Was sollte ich tun, wenn ich zu viel Cannabis konsumiert habe?');
+        $entry->setArticleContent('
+            <p>Wenn Sie zu viel Cannabis konsumiert haben, können folgende Maßnahmen helfen:</p>
+            <ul>
+              <li><strong>Bleiben Sie ruhig:</strong> Panik verschlimmert die Situation. Versuchen Sie, ruhig zu bleiben und tief zu atmen.</li>
+              <li><strong>Hydrieren:</strong> Trinken Sie Wasser, um einen trockenen Mund zu lindern und Ihren Körper zu unterstützen.</li>
+              <li><strong>Essen:</strong> Eine kleine Mahlzeit kann helfen, die Effekte zu mildern.</li>
+              <li><strong>Ruhe:</strong> Legen Sie sich an einen ruhigen Ort und warten Sie ab, bis die Wirkung nachlässt. Dies kann mehrere Stunden dauern.</li>
+              <li><strong>Freunde kontaktieren:</strong> Wenn Sie sich sehr unwohl fühlen, informieren Sie einen Freund oder ein Familienmitglied.</li>
+            </ul>
+        ');
         $manager->persist($entry);
 
         $entry = new KnowledgeBase();
         $entry->setSite('knowledge_base');
-        $entry->setCategorie('cannabis_tips');
-        $entry->setArticleName('How to use cannabis');
-        $entry->setArticleContent('This is how you use cannabis');
+        $entry->setCategorie('consuming_tips');
+        $entry->setArticleName('Kann ich Cannabis mit anderen Substanzen kombinieren?');
+        $entry->setArticleContent('<p>Es ist generell nicht empfehlenswert, Cannabis mit anderen Substanzen zu kombinieren. Mischkonsum kann die Wirkungen und Nebenwirkungen verstärken und unvorhersehbare Reaktionen auslösen. Insbesondere die Kombination mit Alkohol oder anderen psychoaktiven Substanzen kann gefährlich sein.</p>');
         $manager->persist($entry);
 
         $entry = new KnowledgeBase();
         $entry->setSite('knowledge_base');
-        $entry->setCategorie('CannaConsultant');
-        $entry->setArticleName('CannaConsultant');
-        $entry->setArticleContent('This is how you use CannaConsultant');
+        $entry->setCategorie('consuming_tips');
+        $entry->setArticleName('Welche gesetzlichen Regelungen gibt es?');
+        $entry->setArticleContent('<p>Die gesetzlichen Regelungen für Cannabis variieren stark von Land zu Land und sogar von Region zu Region. Es ist wichtig, sich über die spezifischen Gesetze in Ihrem Gebiet zu informieren. In einigen Ländern ist der Konsum von Cannabis vollständig legalisiert, in anderen ist es nur für medizinische Zwecke erlaubt, und in manchen Ländern ist es komplett verboten.</p>');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('consuming_tips');
+        $entry->setArticleName('Welche Gefahren gibt es?');
+        $entry->setArticleContent('
+            <p>Die Gefahren des Cannabiskonsums umfassen sowohl physische als auch psychische Aspekte:</p>
+            <ul>
+              <li><strong>Psychische Gesundheit:</strong> Hohe THC-Dosen können Angstzustände, Paranoia und psychotische Episoden auslösen, insbesondere bei gefährdeten Personen.</li>
+              <li><strong>Abhängigkeit:</strong> Langfristiger, regelmäßiger Konsum kann zu einer psychischen Abhängigkeit führen.</li>
+              <li><strong>Kognitive Funktionen:</strong> Bei Jugendlichen kann der Konsum die Gehirnentwicklung beeinträchtigen und zu langfristigen kognitiven Problemen führen.</li>
+              <li><strong>Körperliche Gesundheit:</strong> Rauchen von Cannabis kann die Lungen schädigen, ähnlich wie Tabakrauch.</li>
+            </ul>
+        ');
         $manager->persist($entry);
 
         $entry = new KnowledgeBase();
@@ -92,6 +234,13 @@ class KnowledgeBaseFixtures extends Fixture
         $entry->setCategorie('GrowMate');
         $entry->setArticleName('Grow Mate');
         $entry->setArticleContent('This is how you use Grow Mate');
+        $manager->persist($entry);
+
+        $entry = new KnowledgeBase();
+        $entry->setSite('knowledge_base');
+        $entry->setCategorie('CannaConsultant');
+        $entry->setArticleName('CannaConsultant');
+        $entry->setArticleContent('This is how you use CannaConsultant');
         $manager->persist($entry);
 
         $manager->flush();
