6ad8934c1fb2a5cb6e78c793de690add32ec5141
jakob-pfefferkorn <jakob.pfefferkorn@vodafone.com>
2024-07-10 15:59:52 +0200

    Intermediary commit

--- assets/script/cannastrainLibrary.js
+++ assets/script/cannastrainLibrary.js
@@ -1,3 +1,9 @@
+document.getElementById('show-sidebar').addEventListener('click', function () {
+    document.getElementById('filters-hidden-container').classList.toggle('d-none');
+    document.getElementById('filters-container').classList.toggle('d-none');
+});
+
 document.getElementById('close-sidebar').addEventListener('click', function () {
     document.getElementById('filters-container').classList.toggle('d-none');
+    document.getElementById('filters-hidden-container').classList.toggle('d-none');
 });
