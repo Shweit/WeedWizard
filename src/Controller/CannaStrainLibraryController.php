817ac2603a03639a6297cf919c3a27d650cdb4a8
jakob-pfefferkorn <jakob.pfefferkorn@vodafone.com>
2024-07-17 16:32:07 +0200

    Make btns to next and previous strain available

--- src/Controller/CannaStrainLibraryController.php
+++ src/Controller/CannaStrainLibraryController.php
@@ -35,13 +35,10 @@ class CannaStrainLibraryController extends AbstractController
     public function index(): Response
     {
         $breeders = $this->seedFinderApiService->getBreederInfo();
-        $totalNumOfBreeders = count($this->seedFinderApiService->getBreederInfoPaginated('all', 1, PHP_INT_MAX));
 
         return $this->render('cannastrain_library/index.html.twig', [
             'breeders' => $breeders,
             'filters' => $this->breederFilters,
-            'totalPages' => ceil($totalNumOfBreeders / $this->paginationLimit),
-            'breeder_id' => null,
         ]);
     }
 
@@ -49,13 +46,11 @@ class CannaStrainLibraryController extends AbstractController
     public function showBreeder(string $breeder_id): Response
     {
         $breeder = $this->seedFinderApiService->getBreederInfo($breeder_id);
-        $totalNumOfStrains = count($breeder['strains']);
 
         return $this->render('cannastrain_library/breeder/showBreeder.html.twig', [
             'breeder' => $breeder,
             'filters' => $this->strainFilters,
             'strains' => $breeder['strains'],
-            'totalPages' => ceil($totalNumOfStrains / $this->paginationLimit),
             'breeder_id' => $breeder['seedfinder_id'],
         ]);
     }
@@ -66,6 +61,11 @@ class CannaStrainLibraryController extends AbstractController
         $breeder = $this->seedFinderApiService->getBreederInfo($breeder_id);
         $strain = $this->seedFinderApiService->getStrainInfo($breeder_id, $strain_id);
 
+        // Other strains links
+        $previousStrain = $this->findPreviousStrain($breeder['strains'], $strain_id);
+        $nextStrain = $this->findNextStrain($breeder['strains'], $strain_id);
+
+        // Selected strain info
         $strainName = $strain['name'];
         $strainInfo = $strain['breeder_info'];
         $strainMedicalInfo = $strain['medical'];
@@ -77,6 +77,42 @@ class CannaStrainLibraryController extends AbstractController
             'strainMedicalInfo' => $strainMedicalInfo,
             'strain' => $strain,
             'breeder' => $breeder,
+            'previousStrainLink' => $this->generateBtnLinkToStrain($breeder_id, $previousStrain['seedfinder_id']),
+            'nextStrainLink' => $this->generateBtnLinkToStrain($breeder_id, $nextStrain['seedfinder_id']),
+        ]);
+    }
+
+    private function findPreviousStrain($allStrainsFromBreeder, string $currentStrainId)
+    {
+        $allStrainIds = array_column($allStrainsFromBreeder, 'seedfinder_id');
+        $currentIndex = array_search($currentStrainId, $allStrainIds);
+
+        // Return last element if current strain is first or not found
+        if ($currentIndex === false || $currentIndex === 0) {
+            return end($allStrainsFromBreeder);
+        }
+
+        return $allStrainsFromBreeder[$currentIndex - 1];
+    }
+
+    private function findNextStrain($allStrainsFromBreeder, string $currentStrainId)
+    {
+        $allStrainIds = array_column($allStrainsFromBreeder, 'seedfinder_id');
+        $currentIndex = array_search($currentStrainId, $allStrainIds);
+
+        // Return first element if current strain is last or not found
+        if ($currentIndex === false || $currentIndex === 0) {
+            return end($allStrainsFromBreeder);
+        }
+
+        return $allStrainsFromBreeder[$currentIndex - 1];
+    }
+
+    private function generateBtnLinkToStrain($breederId, $strainId): string
+    {
+        return $this->generateUrl('weedwizard_cannastrain-library_strain-view', [
+            'breeder_id' => $breederId,
+            'strain_id' => $strainId,
         ]);
     }
 }
