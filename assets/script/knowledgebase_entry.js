074abd91cc995628bbf68e338ba0349ce204754a
Dennis van den Brock <dennis.vandenbrock@easybill.de>
2024-07-22 18:57:39 +0200

    KnowledgeBase Fixes fixed the error in the KnowledgeBase where the sidebar is not fixed on scroll

--- assets/script/knowledgebase_entry.js
+++ assets/script/knowledgebase_entry.js
@@ -1,31 +1,32 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-   // Add the table of contents to the navbar
-   var toc = document.getElementById('articleContent').querySelectorAll('section');
-   var tocNavbar = document.getElementById('knowledgebase_navbar');
+    // Add the table of contents to the navbar
+    var toc = document.getElementById('articleContent').querySelectorAll('section');
+    var tocNavbar = document.getElementById('knowledgebase_navbar');
 
-   toc.forEach(function (element) {
-      var anchor = document.createElement('a');
+    toc.forEach(function (element) {
+        var anchor = document.createElement('a');
 
-      // Add the anchor to the navbar
-      anchor.href = '#' + element.id;
-      anchor.classList.add('nav-link');
-      anchor.textContent = element.dataset.navTitle;
+        // Add the anchor to the navbar
+        anchor.href = '#' + element.id;
+        anchor.classList.add('nav-link');
+        anchor.textContent = element.dataset.navTitle;
 
-      tocNavbar.appendChild(anchor);
-   });
+        tocNavbar.appendChild(anchor);
+    });
 
-   // Make the navbar sticky if it reaches the top of the screen
-   const curStickyPosition = tocNavbar.getBoundingClientRect().top + window.pageYOffset;
-   let knowledgebase_navbar_card = document.getElementById('knowledgebase_navbar_card');
-   const navbarHeight = document.getElementById('navbar').offsetHeight;
-
-    window.onscroll = function() {
-        if (window.pageYOffset > curStickyPosition - navbarHeight) {
-           knowledgebase_navbar_card.style.position = 'fixed';
-           knowledgebase_navbar_card.style.top = navbarHeight + 10 + 'px';
+    // Make the Sidebar with the Content of the Article sticky
+    var sidebar = document.getElementById('knowledgebase_navbar_card');
+    let navbar = document.getElementById('navbar');
+    let activateFixedOn = 47 + navbar.offsetHeight;
+    let width = sidebar.offsetWidth;
+    window.onscroll = function () {
+        if (window.pageYOffset > activateFixedOn) {
+            sidebar.style.top = navbar.offsetHeight + 10 + 'px';
+            sidebar.style.width = width + 'px';
+            sidebar.classList.add('position-fixed');
         } else {
-           knowledgebase_navbar_card.style.position = 'relative';
-              knowledgebase_navbar_card.style.top = '0px';
+            sidebar.style.top = '0px';
+            sidebar.classList.remove('position-fixed');
         }
-    };
+    }
 });
